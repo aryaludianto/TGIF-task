1559a26ac2f7ccabd333ee2b903ff3c3a5b4418c
Arya Ludianto <arya.pratamal@ymail.com>
2019-06-02 22:29:25 +0200

    create party checkbox and filter by state but party checkbox still not done

--- data/pro-congress-113-house.js
+++ data/pro-congress-113-house.js
@@ -20716,10 +20716,255 @@ var data = {
 }
 
 
+let stateAbb = [
+  {
+      "name": "Alabama",
+      "abbreviation": "AL"
+  },
+  {
+      "name": "Alaska",
+      "abbreviation": "AK"
+  },
+  {
+      "name": "American Samoa",
+      "abbreviation": "AS"
+  },
+  {
+      "name": "Arizona",
+      "abbreviation": "AZ"
+  },
+  {
+      "name": "Arkansas",
+      "abbreviation": "AR"
+  },
+  {
+      "name": "California",
+      "abbreviation": "CA"
+  },
+  {
+      "name": "Colorado",
+      "abbreviation": "CO"
+  },
+  {
+      "name": "Connecticut",
+      "abbreviation": "CT"
+  },
+  {
+      "name": "Delaware",
+      "abbreviation": "DE"
+  },
+  {
+      "name": "District Of Columbia",
+      "abbreviation": "DC"
+  },
+  {
+      "name": "Federated States Of Micronesia",
+      "abbreviation": "FM"
+  },
+  {
+      "name": "Florida",
+      "abbreviation": "FL"
+  },
+  {
+      "name": "Georgia",
+      "abbreviation": "GA"
+  },
+  {
+      "name": "Guam",
+      "abbreviation": "GU"
+  },
+  {
+      "name": "Hawaii",
+      "abbreviation": "HI"
+  },
+  {
+      "name": "Idaho",
+      "abbreviation": "ID"
+  },
+  {
+      "name": "Illinois",
+      "abbreviation": "IL"
+  },
+  {
+      "name": "Indiana",
+      "abbreviation": "IN"
+  },
+  {
+      "name": "Iowa",
+      "abbreviation": "IA"
+  },
+  {
+      "name": "Kansas",
+      "abbreviation": "KS"
+  },
+  {
+      "name": "Kentucky",
+      "abbreviation": "KY"
+  },
+  {
+      "name": "Louisiana",
+      "abbreviation": "LA"
+  },
+  {
+      "name": "Maine",
+      "abbreviation": "ME"
+  },
+  {
+      "name": "Marshall Islands",
+      "abbreviation": "MH"
+  },
+  {
+      "name": "Maryland",
+      "abbreviation": "MD"
+  },
+  {
+      "name": "Massachusetts",
+      "abbreviation": "MA"
+  },
+  {
+      "name": "Michigan",
+      "abbreviation": "MI"
+  },
+  {
+      "name": "Minnesota",
+      "abbreviation": "MN"
+  },
+  {
+      "name": "Mississippi",
+      "abbreviation": "MS"
+  },
+  {
+      "name": "Missouri",
+      "abbreviation": "MO"
+  },
+  {
+      "name": "Montana",
+      "abbreviation": "MT"
+  },
+  {
+      "name": "Nebraska",
+      "abbreviation": "NE"
+  },
+  {
+      "name": "Nevada",
+      "abbreviation": "NV"
+  },
+  {
+      "name": "New Hampshire",
+      "abbreviation": "NH"
+  },
+  {
+      "name": "New Jersey",
+      "abbreviation": "NJ"
+  },
+  {
+      "name": "New Mexico",
+      "abbreviation": "NM"
+  },
+  {
+      "name": "New York",
+      "abbreviation": "NY"
+  },
+  {
+      "name": "North Carolina",
+      "abbreviation": "NC"
+  },
+  {
+      "name": "North Dakota",
+      "abbreviation": "ND"
+  },
+  {
+      "name": "Northern Mariana Islands",
+      "abbreviation": "MP"
+  },
+  {
+      "name": "Ohio",
+      "abbreviation": "OH"
+  },
+  {
+      "name": "Oklahoma",
+      "abbreviation": "OK"
+  },
+  {
+      "name": "Oregon",
+      "abbreviation": "OR"
+  },
+  {
+      "name": "Palau",
+      "abbreviation": "PW"
+  },
+  {
+      "name": "Pennsylvania",
+      "abbreviation": "PA"
+  },
+  {
+      "name": "Puerto Rico",
+      "abbreviation": "PR"
+  },
+  {
+      "name": "Rhode Island",
+      "abbreviation": "RI"
+  },
+  {
+      "name": "South Carolina",
+      "abbreviation": "SC"
+  },
+  {
+      "name": "South Dakota",
+      "abbreviation": "SD"
+  },
+  {
+      "name": "Tennessee",
+      "abbreviation": "TN"
+  },
+  {
+      "name": "Texas",
+      "abbreviation": "TX"
+  },
+  {
+      "name": "Utah",
+      "abbreviation": "UT"
+  },
+  {
+      "name": "Vermont",
+      "abbreviation": "VT"
+  },
+  {
+      "name": "Virgin Islands",
+      "abbreviation": "VI"
+  },
+  {
+      "name": "Virginia",
+      "abbreviation": "VA"
+  },
+  {
+      "name": "Washington",
+      "abbreviation": "WA"
+  },
+  {
+      "name": "West Virginia",
+      "abbreviation": "WV"
+  },
+  {
+      "name": "Wisconsin",
+      "abbreviation": "WI"
+  },
+  {
+      "name": "Wyoming",
+      "abbreviation": "WY"
+  }
+]
+
+
 
 let members = data.results[0].members
 
 function dispSenat (member) {
+  while(document.getElementById("senate-data").hasChildNodes())
+  {
+    document.getElementById("senate-data").removeChild(document.getElementById("senate-data").firstChild);
+  }
+
   let senTab = document.getElementById("senate-data");
 
   member.forEach(data => {
@@ -20758,4 +21003,67 @@ function nameVal (data) {
   return link;
 }
 
-$(document).ready(dispSenat(members));
+// display state in the state selection-------
+function dispState () {
+  let selState = document.getElementById("state");
+
+  stateAbb.forEach(state => {
+    let opt = document.createElement("option"),
+    stateSelect = document.createTextNode(state.name);
+    opt.appendChild(stateSelect);
+    opt.value = state.abbreviation;
+
+    return selState.appendChild(opt);
+
+  })
+}
+
+// ---filter data by state ------
+function fillState () {
+  let selectedState = document.getElementById("state"),
+  data;
+
+  if( selectedState.value === "all"){
+    data = members;
+  } else {
+   data = members.filter(data=> {
+   return data.state === selectedState.value
+  });
+}
+ return fillParty(data);
+
+}
+
+//-----Filter data by party 
+
+function fillParty (data) {
+  let rep = document.getElementById("checkRep"),
+    dem = document.getElementById("checkDem"),
+    ind = document.getElementById("checkInd"),
+    dataDisp;
+   
+    
+    
+    
+    if(!rep.checked && !dem.checked && !ind.checked){
+      dataDisp = data;
+    } else if (rep.checked && dem.checked && ind.checked){
+      dataDisp = data;
+    } else if (!rep.checked) {
+      dataDisp = data.filter(member => member.party != rep.value);
+    } else if (!dem.checked){
+      dataDisp = data.filter(member => member.party != dem.value);
+    } else if (!ind.checked) {
+      dataDisp = data.filter(member => member.party != ind.value);
+    } 
+
+    return dispSenat(dataDisp)
+
+}
+
+
+
+
+
+$(document).ready(fillState());
+$(document).ready(dispState);
